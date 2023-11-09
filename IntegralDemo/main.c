ccfcd7733ddae87b1214b613d5259acf4ec9ff6b
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2023-11-09 17:02:26 +0200

    UI bug fixing, report

--- IntegralDemo/main.c
+++ IntegralDemo/main.c
@@ -4,13 +4,14 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
 
-#define E_TEST       0.001
-#define A_TEST       1.0
-#define B_TEST       9.0
-#define S_EXPECTED   172.0
+#define A_TEST     1.0
+#define B_TEST     9.0
+#define S_EXPECTED 172.0
+#define TEST_COUNT 5
 
-#define USAGE_MSG "Usage: test_count:int ([s]ingle-threaded n:int | [m]ulti-threaded segment_count:int thread_count:int)\n"
+#define USAGE_MSG "Usage: [s]ingle-threaded segment_count:int | [m]ulti-threaded segment_count:int thread_count:int\n"
 
 static double f_test(double x);
 static void test(double (*simpson)(double(*)(double), double, double, int));
@@ -19,66 +20,61 @@ static int n = 0;
 
 int main(int argc, char ** argv)
 {
-	if (argc < 4)
+	if (argc < 3)
 	{
 		puts(USAGE_MSG);
 		return 0;
 	}
 
-	int test_count = atoi(argv[1]);
-	if (test_count <= 0)
-	{
-		printf("Invalid test count: %s\n", argv[1]);
-		return 0;
-	}
-
-	switch (argv[2][0])
+	switch (argv[1][0])
 	{
 		case 's':
+
+			if (argc != 3)
+			{
+				puts(USAGE_MSG);
+				return 0;
+			}
+
+			n = atoi(argv[2]);
+			if (n <= 1)
+			{
+				printf("Invalid segment count: %s\n", argv[2]);
+				return 0;
+			}
+
+			test(simpson_1t);
+
+			break;
+
+		case 'm':
+
 			if (argc != 4)
 			{
 				puts(USAGE_MSG);
 				return 0;
 			}
-			n = atoi(argv[3]);
+
+			n = atoi(argv[2]);
 			if (n <= 1)
 			{
-				printf("Invalid segment count: %s\n", argv[3]);
+				printf("Invalid segment count: %s\n", argv[2]);
 				return 0;
 			}
-			for (int i = 0; i < test_count; i++)
+
+			int num_threads = atoi(argv[3]);
+			if (num_threads <= 0)
 			{
-				printf("Single-thread simpson demo #%d\n", i);
-				test(simpson_1t);
-				printf("Single-thread simpson demo #%d end\n\n", i);
-			}
-			break;
-		case 'm':
-			if (argc != 5)
-			{
-				puts(USAGE_MSG);
+				printf("Invalid thread count: %s\n", argv[3]);
 				return 0;
 			}
-			n = atoi(argv[3]);
-			if (n <= 1)
-			{
-				printf("Invalid segment count: %s\n", argv[3]);
-				return 0;
-			}
-			int num_threads = atoi(argv[4]);
-			if (n <= 0)
-			{
-				printf("Invalid segment count: %s\n", argv[1]);
-				return 0;
-			}
+
 			omp_set_num_threads(num_threads);
-			for (int i = 0; i < test_count; i++)
-			{
-				printf("Multi-thread simpson demo #%d\n", i);
-				test(simpson_mt);
-				printf("Multi-thread simpson demo #%d end\n\n", i);
-			}
+
+			test(simpson_mt);
+
 			break;
+
 		default:
 			puts(USAGE_MSG);
 			return 0;
@@ -89,21 +85,28 @@ int main(int argc, char ** argv)
 
 static void test(double (*simpson)(double(*)(double), double, double, int))
 {
-	// Calculating integral
-	double s_time = omp_get_wtime();
-	double s_actual = simpson(f_test, A_TEST, B_TEST, n);
-	double e_time = omp_get_wtime();
+	double s_time, e_time, time, min_time = DBL_MAX;
+	double integral;
+	for (int i = 0; i < TEST_COUNT; i++)
+	{
+		// Calculating integral
+		s_time = omp_get_wtime();
+		integral = simpson(f_test, A_TEST, B_TEST, n);
+		e_time = omp_get_wtime();
 
-	// Asserting
-	printf("s_actual = %lf, time = %lf | n = %d\n", s_actual, e_time - s_time, n);
-	if (fabs(s_actual - S_EXPECTED) <= E_TEST)
-		puts("Assertion succeeded");
-	else
-		puts("Assertion failed");
+		// Asserting
+		time = e_time - s_time;
+		printf("s_actual = %lf, time = %lf, diff = %lf\n", integral, time, fabs(integral - S_EXPECTED));
+
+		// Comparing
+		if (min_time > time)
+			min_time = time;
+	}
+	printf("min time = %lf\n", min_time);
 }
 
 static double f_test(double x)
 {
-	long double sqrt_x = sqrt(x);
+	double sqrt_x = sqrt(x);
 	return 3.0 * sqrt_x * (1 + sqrt_x);
 }
